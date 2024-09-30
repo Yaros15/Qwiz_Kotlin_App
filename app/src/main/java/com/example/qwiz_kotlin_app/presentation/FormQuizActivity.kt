3b599c2050c3b1e8dc47a47015a41b55b5b6539d
Yaros15 <chaos0909@mail.ru>
2024-09-30 12:25:02 +0400

    master

--- app/src/main/java/com/example/qwiz_kotlin_app/presentation/FormQuizActivity.kt
+++ app/src/main/java/com/example/qwiz_kotlin_app/presentation/FormQuizActivity.kt
@@ -1,6 +1,49 @@
 package com.example.qwiz_kotlin_app.presentation
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.qwiz_kotlin_app.R
+import com.example.qwiz_kotlin_app.domain.GameplayQuiz
 
 class FormQuizActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_quiz)
+
+        val gameplayQuiz : GameplayQuiz = GameplayQuiz()
+
+        val questionText : TextView = findViewById(R.id.question)
+        val answerText : EditText = findViewById(R.id.answerText)
+        val answerButton : Button = findViewById(R.id.answer)
+        val nextButton : Button = findViewById(R.id.next)
+
+        val correctly = getText(R.string.correctly)
+        val wrong = getText(R.string.wrong)
+
+        questionText.text = gameplayQuiz.getNamePazzleQuestion()
+
+        answerButton.setOnClickListener{
+
+            var resul: Boolean = gameplayQuiz.checkUsersResponse(answerText.text.toString())
+
+            if(resul){
+                questionText.text = correctly
+            }else{
+                questionText.text = wrong
+            }
+
+        }
+
+        nextButton.setOnClickListener {
+
+            questionText.text = gameplayQuiz.getNamePazzleQuestion()
+
+        }
+
+    }
+
 }
